40d1a09086c1f7366f0a97620c04eadff3cd9f09
Dilpreet Singh <dilpreet@vinsol.com>
2014-07-22 19:49:56 +0530

    Modifying the vaildate functions by breaking down all the individual validators

--- form_validation.js
+++ form_validation.js
@@ -1,59 +1,94 @@
-function formFieldChecker() {};
+function formFieldChecker(getElements) {
+  this.loginElement = getElements.loginEl;
+  this.emailElement = getElements.emailEl;
+  this.nameElement = getElements.nameEl;
+  this.homePageElement = getElements.homePageEl;
+  this.selectElement = getElements.selectEl;
+  this.textAreaElement = getElements.textAreaEl;
+  this.goButtonElement = getElements.goButtonEl;
+  this.recieveChkElement = getElements.recieveChkEl;
+  this.valid = true;
+};
 formFieldChecker.prototype.validate = function() {
-  if(!loginEl.value) {
-    alert("Login id cannot be empty");
-    return false;
+  if(this.isLoginIdValid() || this.isEmailValid() || this.isNameValid() || this.isTimezoneSelected() || this.isHomePageValid() || this.isTextareaValid() || this.isChecked()) {
+    this.valid = false;
   }
-  else if ((!emailEl.value) || (!emailpatt.test(emailEl.value))) {
+  return this.valid;
+};
+
+formFieldChecker.prototype.isEmailValid = function() {
+  if ((!this.emailElement.value) || (!EMAIL_PATT.test(this.emailElement.value))) {
     alert("Invaild Email id");
     return false;
   }
-  else if ((!nameEl.value) || (!namepatt.test(nameEl.value))) {
+};
+
+formFieldChecker.prototype.isNameValid = function() {
+  if ((!this.nameElement.value) || (!NAME_PATT.test(this.nameElement.value))) {
     alert("Invaild name");
     return false;
   }
-  else if (!(selectEl.options.selectedIndex > 0)) {
+};
+
+formFieldChecker.prototype.isHomePageValid = function() {
+  if ((!this.homePageElement.value) || (!URL_PATT.test(this.homePageElement.value))) {
+    alert("Please Enter valid Home Page URl");
+    return false;
+  }
+};
+
+formFieldChecker.prototype.isTimezoneSelected = function() {
+  if (!(this.selectElement.options.selectedIndex > 0)) {
     alert("Please Choose Timezone");
     return false;
   }
-  else if ((!homePageEl.value) || (!urlpatt.test(homePageEl.value))) {
-    alert("Please Enter valide Home Page URl");
-  }
-  else if ((!textareaEl.value) || (textareaEl.value.length < 50)){
+};
+
+formFieldChecker.prototype.isTextareaValid = function() {
+  if ((!this.textAreaElement.value) || (this.textAreaElement.value.length < 50)) {
     alert("Please Enter details in about me for about 50 characters");
     return false;
   }
-  else if (!recieveChkEl.checked) {
+};
+
+formFieldChecker.prototype.isChecked = function() {
+  if (!this.recieveChkElement.checked) {
     alert("Please check recieve Notification");
     return false;
   }
-  return true;
 };
+
+formFieldChecker.prototype.isLoginIdValid = function() {
+  if (!this.loginElement.value) {
+    alert("Login id cannot be empty");
+    return false;
+  }
+};
+
 formFieldChecker.prototype.bindEvents = function() {
   var _this = this;
-  goButtonEl.addEventListener("click",function(e){
+  this.goButtonElement.addEventListener("click",function(e){
     var result = _this.validate();
     if(!result) {
       e.stopPropagation();
     }
   });
 };
-function createFormChecker() {
-	var formChecker = new formFieldChecker();
+function createFormChecker(getElements) {
+	var formChecker = new formFieldChecker(getElements);
   formChecker.bindEvents();
 }
-var loginEl = document.getElementById("loginId"),
-    emailEl = document.getElementById("email"),
-    nameEl = document.getElementById("name"),
-    homePageEl = document.getElementById("HomePage"),
-    selectEl = document.getElementById("selectBox"),
-    textareaEl = document.getElementById("textarea"),
-    goButtonEl = document.getElementById("button"),
-    recieveChkEl = document.getElementById("recieveNotification"),
-    urlStr ="(ftp:\/\/\/|http:\/\/|https:\/\/)(w{3}|[a-zA-Z]*)\.([a-zA-Z]*|\d)\.([a-zA-Z]*|\d|\.([a-zA-Z]*|\d)|\/([a-zA-Z]*|\d))",
-    urlpatt = new RegExp(urlStr),
-    emailStr = "([a-z]|\d|.|#|$|)*@([a-z]|\d)*.([a-z][a-z][a-z]|[a-z][a-z].[a-z][a-z])",
-    emailpatt = new RegExp(emailStr),
-    nameStr = "([a-zA-Z][a-zA-Z]*)",
-    namepatt = new RegExp(nameStr);
-window.onload= createFormChecker();
+var elements = {
+  "loginEl" : document.getElementById("loginId"),
+  "emailEl" : document.getElementById("email"),
+  "nameEl" : document.getElementById("name"),
+  "homePageEl" : document.getElementById("HomePage"),
+  "selectEl" : document.getElementById("selectBox"),
+  "textAreaEl" : document.getElementById("textarea"),
+  "goButtonEl" : document.getElementById("button"),
+  "recieveChkEl" : document.getElementById("recieveNotification")
+}
+var URL_PATT = new RegExp("(ftp:\/\/\/|http:\/\/|https:\/\/)(w{3}|[a-zA-Z]*)\.([a-zA-Z]*|\d)\.([a-zA-Z]*|\d|\.([a-zA-Z]*|\d)|\/([a-zA-Z]*|\d))"),
+    EMAIL_PATT = new RegExp("([a-z]|\d|.|#|$|)*@([a-z]|\d)*.([a-z][a-z][a-z]|[a-z][a-z].[a-z][a-z])"),
+    NAME_PATT = new RegExp("([a-zA-Z][a-zA-Z]*)");
+window.onload= createFormChecker(elements);
